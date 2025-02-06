ecf77d9dc99822571ec60f22c89a772b45cffac1
Heinz K <heka@techcollege.dk>
2025-02-06 12:45:24 +0100

    CRUD functions

--- controller/carController.js
+++ controller/carController.js
@@ -7,11 +7,11 @@ export const carController = express.Router()
 carController.get('/cars', async (req, res) => {
     try {
         const data = await carModel.findAll({
-            attributes: ['brand', 'color']
+            attributes: ['id', 'brand', 'color']
         })
 
-        if(!data || data.length === 0) {
-            return res.json({ message: 'No data found'})
+        if (!data || data.length === 0) {
+            return res.json({ message: 'No data found' })
         }
         res.json(data)
     } catch (error) {
@@ -23,29 +23,29 @@ carController.get('/cars', async (req, res) => {
 carController.get('/cars/:id([0-9]*)', async (req, res) => {
     try {
         const { id } = req.params
-        const data = await carModel.findOne({ 
-            where: { 
-                id: id 
+        const data = await carModel.findOne({
+            where: {
+                id: id
             },
             attributes: ['brand', 'color']
         })
 
-        if(!data) {
+        if (!data) {
             return res.json({ message: `Could not find car on id #${id}` })
         }
 
         return res.json(data);
-    
+
     } catch (error) {
-        console.error(`Could not get car details: ${error}`)        
+        console.error(`Could not get car details: ${error}`)
     }
 })
 
 // Route to create (CREATE)
 carController.post('/cars', async (req, res) => {
     const { brand, model, year, price, color } = req.body;
-    
-    if(!brand || !model || !year || !price || !color) {
+
+    if (!brand || !model || !year || !price || !color) {
         return res.json({ message: 'Missing required data' })
     }
 
@@ -56,7 +56,49 @@ carController.post('/cars', async (req, res) => {
 
         res.status(201).json(result)
     } catch (error) {
-        return res.json({ message: `Could not create car: ${error.message}`})
+        return res.json({ message: `Could not create car: ${error.message}` })
+    }
+})
+
+carController.put('/cars', async (req, res) => {
+    const { brand, model, year, price, color, id } = req.body;
+
+    if (!id || !brand || !model || !year || !price || !color) {
+        return res.json({ message: 'Missing required data' })
+    }
+
+    try {
+        const result = await carModel.update({
+            brand, model, year, price, color
+        },
+            { where: { id } }
+        )
+        if(result) {
+            res.json({ message: `Updated id#${id}`})
+        }
+    } catch (error) {
+        return res.json({ message: `Could not update car: ${error.message}` })
+    }
+})
+
+carController.delete('/cars/:id([0-9]*)', async (req, res) => {
+    const { id } = req.params
+    
+    if(id) {
+        try {
+            await carModel.destroy({
+                where: { id }
+            })
+            res.send({
+                message: `Record #${id} deleted`
+            })
+        } catch (error) {
+            res.send(`Error! Could not delete car: ${error}`)
+        }
+    } else {
+        res.send({
+            message: 'Id not valid'
+        })
     }
     
     
